7f14013ab17bb6dd9183bb294911e7a31a8b118b
Leonardo <leonardoar2@al.insper.edu.br>
2022-11-13 17:59:58 -0300

    Término do inicio

--- Jogo.py
+++ Jogo.py
@@ -300,5 +300,9 @@ print('Olá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer
 
 nome = input('Qual seu nome?')
 
-print(f'\nOk {nome}, você tem direito a pular 3 vezes e 2 ajudas!\nAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n\nAperte ENTER para continuar...'  )
-    
+print(f'\nOk {nome}, você tem direito a pular 3 vezes e 2 ajudas!\nAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n\n'  )
+input('Aperte ENTER para continuar...')
+
+
+print('\nO jogo já vai começar! Lá vem a primeira questão!\n\nVamos começar com questões do nível FACIL!\n')  
+input('Aperte ENTER para continuar...')
